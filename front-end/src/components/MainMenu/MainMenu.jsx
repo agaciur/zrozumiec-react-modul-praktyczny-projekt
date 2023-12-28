4476c5eebe036499a7e4d3f4c319f3b50075291c
Agata Rojewska <agata.rojewska@gmail.com>
2023-12-28 12:56:02 +0100

    Finish lesson 7

---
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -0,0 +1,14 @@
+import styles from "./MainMenu.module.css"
+import { NavLink } from "react-router-dom"
+import { GENDERS } from "../../constants/categories"
+export function MainMenu() {
+  return (
+    <ul className={styles.mainMenu}>
+      {GENDERS.map(gender => (
+        <li key={gender.path}>
+          <NavLink to={gender.path}>{gender.categoryName}</NavLink>
+        </li>
+      ))}
+    </ul>
+  )
+}
