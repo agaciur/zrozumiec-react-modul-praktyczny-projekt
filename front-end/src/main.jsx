a5733258d888ab463ac4f6108dc7cd368637724e
Agata Rojewska <agata.rojewska@gmail.com>
2024-01-06 15:05:43 +0100

    Downloading product

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -6,9 +6,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Favourites } from "./views/Favourites.jsx"
 import { Cart } from "./views/Cart.jsx"
 import { ProductDetails } from "./views/ProductDetails.jsx"
+import { ProductsList } from "./views/ProductsList.jsx"
 import { Layout } from "./components/Layout/Layout.jsx"
 import { MainPage } from "./views/MainPage.jsx"
 import { mainPageLoader } from "./api/mainPageLoader.js"
+import { productListLoader } from "./api/productListLoader.js"
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,11 @@ const router = createBrowserRouter([
         path: "/:gender?",
         element: <MainPage />,
         loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
       },
     ],
   },
